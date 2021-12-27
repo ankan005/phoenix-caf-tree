c4716547e8f27233a22cbb95350ef14b21ce9fdc
ankan005 <ankan005@gmail.com>
2021-12-27 21:00:36 +0000

    renamed

--- device.mk
+++ device.mk
@@ -425,7 +425,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-pa
+    $(LOCAL_PATH)/overlay-cafex
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
