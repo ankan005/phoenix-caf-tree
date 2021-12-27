74a4ea58ff71f53a69ea5e58f593d9357d971876
ankan005 <ankan005@gmail.com>
2021-12-27 15:05:29 +0000

    parts

--- parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
+++ parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018,2020 The LineageOS Project
+ * Copyright (C) 2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,50 +16,57 @@
 
 package org.lineageos.settings.dirac;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Switch;
-
-import androidx.preference.ListPreference;
 import androidx.preference.Preference;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragment;
 import androidx.preference.SwitchPreference;
-
 import com.android.settingslib.widget.MainSwitchPreference;
 import com.android.settingslib.widget.OnMainSwitchChangeListener;
 
 import org.lineageos.settings.R;
 
 public class DiracSettingsFragment extends PreferenceFragment implements
-        Preference.OnPreferenceChangeListener, OnMainSwitchChangeListener {
+        OnPreferenceChangeListener, OnMainSwitchChangeListener {
 
     private static final String PREF_ENABLE = "dirac_enable";
     private static final String PREF_HEADSET = "dirac_headset_pref";
+    private static final String PREF_HIFI = "dirac_hifi_pref";
     private static final String PREF_PRESET = "dirac_preset_pref";
 
     private MainSwitchPreference mSwitchBar;
 
     private ListPreference mHeadsetType;
     private ListPreference mPreset;
+    private SwitchPreference mHifi;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.dirac_settings);
-
-        DiracUtils.initialize();
-        boolean enhancerEnabled = DiracUtils.isDiracEnabled(getActivity());
-
+        boolean enhancerEnabled = DiracUtils.isDiracEnabled();
         mSwitchBar = (MainSwitchPreference) findPreference(PREF_ENABLE);
         mSwitchBar.addOnSwitchChangeListener(this);
         mSwitchBar.setChecked(enhancerEnabled);
 
         mHeadsetType = (ListPreference) findPreference(PREF_HEADSET);
         mHeadsetType.setOnPreferenceChangeListener(this);
-        mHeadsetType.setEnabled(enhancerEnabled);
 
         mPreset = (ListPreference) findPreference(PREF_PRESET);
         mPreset.setOnPreferenceChangeListener(this);
-        mPreset.setEnabled(enhancerEnabled);
+
+        mHifi = (SwitchPreference) findPreference(PREF_HIFI);
+        mHifi.setOnPreferenceChangeListener(this);
+
+        boolean hifiEnable = DiracUtils.getHifiMode();
+        mHeadsetType.setEnabled(!hifiEnable && enhancerEnabled);
+        mPreset.setEnabled(!hifiEnable && enhancerEnabled);
     }
 
     @Override
@@ -68,11 +75,17 @@ public class DiracSettingsFragment extends PreferenceFragment implements
             case PREF_HEADSET:
                 DiracUtils.setHeadsetType(Integer.parseInt(newValue.toString()));
                 return true;
-            case PREF_PRESET:
-                DiracUtils.setLevel(String.valueOf(newValue));
+            case PREF_HIFI:
+                DiracUtils.setHifiMode((Boolean) newValue ? 1 : 0);
+                if (DiracUtils.isDiracEnabled()) {
+                    mHeadsetType.setEnabled(!(Boolean) newValue);
+                    mPreset.setEnabled(!(Boolean) newValue);
+                }
                 return true;
-            default:
-                return false;
+            case PREF_PRESET:
+                DiracUtils.setLevel((String) newValue);
+                return true;
+            default: return false;
         }
     }
 
@@ -82,7 +95,9 @@ public class DiracSettingsFragment extends PreferenceFragment implements
 
         DiracUtils.setMusic(isChecked);
 
-        mHeadsetType.setEnabled(isChecked);
-        mPreset.setEnabled(isChecked);
+        if (!DiracUtils.getHifiMode()) {
+            mHeadsetType.setEnabled(isChecked);
+            mPreset.setEnabled(isChecked);
+        }
     }
 }
--- parts/src/org/lineageos/settings/dirac/DiracSound.java
+++ parts/src/org/lineageos/settings/dirac/DiracSound.java
@@ -25,13 +25,19 @@ public class DiracSound extends AudioEffect {
     private static final int DIRACSOUND_PARAM_HEADSET_TYPE = 1;
     private static final int DIRACSOUND_PARAM_EQ_LEVEL = 2;
     private static final int DIRACSOUND_PARAM_MUSIC = 4;
+    private static final int DIRACSOUND_PARAM_HIFI = 8;
 
     private static final UUID EFFECT_TYPE_DIRACSOUND =
-            UUID.fromString("e069d9e0-8329-11df-9168-0002a5d5c51b");
+            UUID.fromString("5b8e36a5-144a-4c38-b1d7-0002a5d5c51b");
     private static final String TAG = "DiracSound";
 
     public DiracSound(int priority, int audioSession) {
         super(EFFECT_TYPE_NULL, EFFECT_TYPE_DIRACSOUND, priority, audioSession);
+    }
+
+    public void setMusic(int enable) throws IllegalStateException,
+            IllegalArgumentException, UnsupportedOperationException {
+        checkStatus(setParameter(DIRACSOUND_PARAM_MUSIC, enable));
     }
 
     public int getMusic() throws IllegalStateException,
@@ -41,36 +47,19 @@ public class DiracSound extends AudioEffect {
         return value[0];
     }
 
-    public void setMusic(int enable) throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
-        checkStatus(setParameter(DIRACSOUND_PARAM_MUSIC, enable));
-    }
-
-    public int getHeadsetType() throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
-        int[] value = new int[1];
-        checkStatus(getParameter(DIRACSOUND_PARAM_HEADSET_TYPE, value));
-        return value[0];
-    }
-
     public void setHeadsetType(int type) throws IllegalStateException,
             IllegalArgumentException, UnsupportedOperationException {
         checkStatus(setParameter(DIRACSOUND_PARAM_HEADSET_TYPE, type));
-    }
-
-    public float getLevel(int band) throws IllegalStateException,
-            IllegalArgumentException, UnsupportedOperationException {
-        int[] param = new int[2];
-        byte[] value = new byte[10];
-        param[0] = DIRACSOUND_PARAM_EQ_LEVEL;
-        param[1] = band;
-        checkStatus(getParameter(param, value));
-        return new Float(new String(value)).floatValue();
     }
 
     public void setLevel(int band, float level) throws IllegalStateException,
             IllegalArgumentException, UnsupportedOperationException {
         checkStatus(setParameter(new int[]{DIRACSOUND_PARAM_EQ_LEVEL, band},
                 String.valueOf(level).getBytes()));
+    }
+
+    public void setHifiMode(int mode) throws IllegalStateException,
+            IllegalArgumentException, UnsupportedOperationException {
+        checkStatus(setParameter(DIRACSOUND_PARAM_HIFI, mode));
     }
 }
