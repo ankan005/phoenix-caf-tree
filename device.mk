ff4976884ed0049a44f2bc83bd86d4ae04104938
ankan005 <ankan005@gmail.com>
2021-12-27 18:32:41 +0000

    name

--- device.mk
+++ device.mk
@@ -646,8 +646,6 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
  PRODUCT_PACKAGES += \
-#    android.hardware.vibrator@1.0-impl \
-#    android.hardware.vibrator@1.0-service
      vendor.qti.hardware.vibrator.service.xiaomi_phoenix
 
 # Binder
